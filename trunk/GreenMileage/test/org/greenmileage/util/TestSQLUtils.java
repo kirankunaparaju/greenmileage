fab8ae9f3bcf3ace536d1b2ddca2fbb2661da5ae
mrsqueezles <mrsqueezles@164b87ce-2a34-11de-b161-0f7b0ef88f65>
2009-05-19 15:12:16 +0000

    Began SQL utils to support construction of where clauses Formatted

---
+++ trunk/GreenMileage/test/org/greenmileage/util/TestSQLUtils.java
@@ -0,0 +1,16 @@
+package org.greenmileage.util;
+
+import org.junit.Test;
+
+/**
+ * @see SQLUtils
+ * @author Connor Garvey
+ * @created May 19, 2009 10:09:39 AM
+ * @version 0.0.5
+ * @since 0.0.5
+ */
+public class TestSQLUtils {
+  @Test
+  public void testBuildWhere() {
+  }
+}
