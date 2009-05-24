d7964a59648b12505dae1c12a650c511eedca8ce
mrsqueezles <mrsqueezles@164b87ce-2a34-11de-b161-0f7b0ef88f65>
2009-05-24 05:39:16 +0000

    Added test cases

--- GreenMileage/src/org/greenmileage/util/ContentValuesUtils.java
+++ GreenMileage/src/org/greenmileage/util/ContentValuesUtils.java
@@ -13,36 +13,36 @@ import android.content.ContentValues;
  * @since 0.0.1
  */
 public class ContentValuesUtils {
-  
   /**
-   * Replaces the value at the specified key with null if the value is an empty
-   * string.  If the key doesn't exist, this method will not have an effect.
+   * Replaces the value at the specified key with null if the value is an empty string. If the key
+   * doesn't exist, this method will not have an effect.
    * @param values The content values
    * @param key The key at which to perform the replacement
    * @throws ClassCastException Thrown if the value at the key is not a string
    */
-  public static void nullEmpty(ContentValues values, String key) throws
-      ClassCastException {
-    Object o = values.get(key);
+  public static void nullEmpty(final ContentValues values, final String key)
+      throws ClassCastException {
+    if (values == null) {
+      throw new IllegalArgumentException("values can't be null");
+    }
+    final Object o = values.get(key);
     if (o == null) {
       return;
     }
-    String value = (String)o;
+    final String value = (String)o;
     values.put(key, StringUtils.nullEmpty(value));
   }
   
   /**
-   * Replaces the value at the specified keys with null if the value is an empty
-   * string.  If any of the keys don't exist, this method will not have an
-   * effect.
+   * Replaces the value at the specified keys with null if the value is an empty string. If any of
+   * the keys don't exist, this method will not have an effect.
    * @param values The content values
    * @param keys The keys at which to perform the replacement
-   * @throws ClassCastException Thrown if any of the values at the keys are not
-   *    strings
+   * @throws ClassCastException Thrown if any of the values at the keys are not strings
    */
-  public static void nullEmpty(ContentValues values, String ... keys) throws
-      ClassCastException {
-    for (String key : keys) {
+  public static void nullEmpty(final ContentValues values, final String... keys)
+      throws ClassCastException {
+    for (final String key : keys) {
       nullEmpty(values, key);
     }
   }
--- GreenMileage/test/src/org/greenmileage/util/TestArrayUtils.java
+++ GreenMileage/test/src/org/greenmileage/util/TestArrayUtils.java
@@ -6,8 +6,8 @@ import junit.framework.TestCase;
  * @see ArrayUtils
  * @author Connor Garvey
  * @created May 22, 2009 9:56:40 AM
- * @version 0.0.1
- * @since 0.0.1
+ * @version 0.0.5
+ * @since 0.0.5
  */
 public class TestArrayUtils extends TestCase {
   /**
