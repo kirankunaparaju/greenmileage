a9d3eed8ce8b280dabd1c4154811af9fd72eed5b
mrsqueezles <mrsqueezles@164b87ce-2a34-11de-b161-0f7b0ef88f65>
2009-05-30 04:14:46 +0000

    Updated to support more tests

--- GreenMileage/src/org/greenmileage/ui/TextViewCallbackListener.java
+++ GreenMileage/src/org/greenmileage/ui/TextViewCallbackListener.java
@@ -7,24 +7,28 @@ import org.greenmileage.util.CallbackListener;
  * Receives a string callback and applies it as the text of a text view
  * @author Connor Garvey
  * @created Jan 18, 2009, 4:26:23 PM
- * @version 0.0.4
+ * @version 0.0.5
  * @since 0.0.4
  */
 public class TextViewCallbackListener implements CallbackListener<String> {
-  private TextView textView;
+  private final TextView textView;
   
   /**
    * Creates a text view callback listener
    * @param textView The text view to receive the response
    */
-  public TextViewCallbackListener(TextView textView) {
+  public TextViewCallbackListener(final TextView textView) {
+    if (textView == null) {
+      throw new IllegalArgumentException("textView can't be null");
+    }
     this.textView = textView;
   }
+  
   /**
    * @see org.greenmileage.util.CallbackListener#onResult(java.lang.Object)
    */
   @Override
-  public void onResult(String result) {
+  public void onResult(final String result) {
     this.textView.setText(result);
   }
 }
