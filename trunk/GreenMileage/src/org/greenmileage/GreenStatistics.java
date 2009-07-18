63f275e6d6d641cb0cae3d404a70196eefbd0441
mrsqueezles <mrsqueezles@164b87ce-2a34-11de-b161-0f7b0ef88f65>
2009-07-18 20:17:32 +0000

    Cleaned up the statistics display class and added display of total distance

--- trunk/GreenMileage/src/org/greenmileage/GreenStatistics.java
+++ trunk/GreenMileage/src/org/greenmileage/GreenStatistics.java
@@ -1,13 +1,12 @@
 package org.greenmileage;
 
-import android.os.Handler;
-import android.database.ContentObserver;
-import android.database.Cursor;
-import android.net.Uri;
-import org.greenmileage.ui.FillupFormat;
-import android.widget.TextView;
-import android.os.Bundle;
 import android.app.Activity;
+import android.database.ContentObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+import org.greenmileage.data.FillupStatistics;
+import org.greenmileage.data.util.FillupStatisticsUtils;
 
 /**
  * Displays overall statistics
@@ -17,15 +16,16 @@ import android.app.Activity;
  * @since 0.0.3
  */
 public class GreenStatistics extends Activity {
-  private ContentObserver observer;
+  private final ContentObserver observer;
+  private TextView totalDistance;
+  private TextView totalMileage;
   
   /**
    * Creates green statistics
    */
   public GreenStatistics() {
-    Handler handler = new Handler();
-    this.observer = new ContentObserver(handler){
-
+    final Handler handler = new Handler();
+    this.observer = new ContentObserver(handler) {
       /**
        * @see android.database.ContentObserver#deliverSelfNotifications()
        */
@@ -33,79 +33,35 @@ public class GreenStatistics extends Activity {
       public boolean deliverSelfNotifications() {
         return false;
       }
-
+      
       /**
        * @see android.database.ContentObserver#onChange(boolean)
        */
       @Override
-      public void onChange(boolean selfChange) {
+      public void onChange(final boolean selfChange) {
       }
-      
     };
   }
   
-  private String findTotalMPG() {
-    // TODO: This is really ugly.  These are separate queries because the mins and maxes include the
-    // earliest fillup.  Sums don't.
-    Uri.Builder builder = new Uri.Builder();
-    builder.scheme("content");
-    builder.authority(GreenMileageDefinitions.AUTHORITY);
-    builder.appendPath("fillups");
-    builder.appendPath("statistics");
-    builder.appendPath("minsmaxes");
-    // These lines are included because Google's code is shit.  If you don't
-    // call these set methods, the generated Uri will throw
-    // NullPointerExceptions
-    builder.query(null);
-    builder.fragment(null);
-    Uri uri = builder.build();
-    this.getContentResolver().registerContentObserver(uri, false, this.
-        observer);
-    Cursor c = this.managedQuery(uri, new String[] {"minMileage", "maxMileage"}, null, null, null);
-    if (c.getCount() == 0) {
-      // TODO: This should be an error message
-      return "";
-    }
-    c.moveToFirst();
-    int minMileage = c.getInt(0);
-    int maxMileage = c.getInt(1);
-    builder = new Uri.Builder();
-    builder.scheme("content");
-    builder.authority(GreenMileageDefinitions.AUTHORITY);
-    builder.appendPath("fillups");
-    builder.appendPath("statistics");
-    builder.appendPath("totals");
-    // These lines are included because Google's code is shit.  If you don't
-    // call these set methods, the generated Uri will throw
-    // NullPointerExceptions
-    builder.query(null);
-    builder.fragment(null);
-    uri = builder.build();
-    this.getContentResolver().registerContentObserver(uri, false, this.
-        observer);
-    c = this.managedQuery(uri, new String[] {"sumVolume"}, null, null, null);
-    if (c.getCount() == 0) {
-      // TODO: This should be an error message
-      return "";
-    }
-    c.moveToFirst();
-    double volume = c.getDouble(0);
-    int mileage = maxMileage - minMileage;
-    if ((volume <= 0) || (mileage <= 0)) {
-      return "";
-    }
-    float totalMPG = (float)(mileage / volume);
-    return FillupFormat.formatMPG(totalMPG);
+  private void findComponents() {
+    this.totalDistance = this.findTextView(R.id.distance);
+    this.totalMileage = this.findTextView(R.id.distancePerVolume);
+  }
+  
+  private TextView findTextView(final int id) {
+    return (TextView)this.findViewById(id);
   }
   
   /**
    * @see android.app.Activity#onCreate(android.os.Bundle)
    */
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  protected void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     this.setContentView(R.layout.green_statistics);
-    TextView totalMileage = (TextView)this.findViewById(R.id.distancePerVolume);
-    totalMileage.setText(this.findTotalMPG());
+    this.findComponents();
+    final FillupStatistics statistics = FillupStatisticsUtils.findStatistics(this, this.observer);
+    this.totalMileage.setText(statistics.getDistancePerVolume());
+    this.totalDistance.setText(statistics.getDistance());
   }
 }
--- trunk/GreenMileage/src/org/greenmileage/data/FillupStatistics.java
+++ trunk/GreenMileage/src/org/greenmileage/data/FillupStatistics.java
@@ -1,36 +1,41 @@
 package org.greenmileage.data;
 
-import org.greenmileage.ui.FillupFormat;
-
 /**
- * Calculates statistical information about {@link Fillup}s
+ * Holds statistics about fillups
  * @author Connor Garvey
- * @created Nov 24, 2008, 12:38:21 PM
- * @version 0.0.3
- * @since 0.0.2
+ * @created Jul 18, 2009 11:27:01 AM
+ * @version 0.0.5
+ * @since 0.0.5
  */
 public class FillupStatistics {
+  private String distance;
+  private String distancePerVolume;
+  
   /**
-   * Calculates the distance per volume between fillups, assuming that the
-   * mileage of the second fillup is greater than the first
-   * @param f1 The first fillup
-   * @param f2 The second fillup
-   * @return The distance per volume
+   * @return the distance
    */
-  public static double distancePerVolume(Fillup f1, Fillup f2) {
-    int distance = f2.getMileage() - f1.getMileage();
-    double dpv = distance / f2.getVolume().doubleValue();
-    return dpv;
+  public String getDistance() {
+    return this.distance;
   }
   
   /**
-   * Calculates the distance per volume between fillups, assuming that the
-   * mileage of the second fillup is greater than the first
-   * @param f1 The first fillup
-   * @param f2 The second fillup
-   * @return The distance per volume as a two decimal string
+   * @return the distancePerVolume
    */
-  public static String distancePerVolumeAsString(Fillup f1, Fillup f2) {
-    return FillupFormat.MPG_FORMAT.format(distancePerVolume(f1, f2));
+  public String getDistancePerVolume() {
+    return this.distancePerVolume;
+  }
+  
+  /**
+   * @param distance the distance to set
+   */
+  public void setDistance(final String distance) {
+    this.distance = distance;
+  }
+  
+  /**
+   * @param distancePerVolume the distancePerVolume to set
+   */
+  public void setDistancePerVolume(final String distancePerVolume) {
+    this.distancePerVolume = distancePerVolume;
   }
 }
